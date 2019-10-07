6b34d85c40a1a2fa0acbdadbe9b18852d308065a
nitro41992 <nick.k@iton.net>
2019-10-06 21:51:33 -0400

    Task: Calculating Confidences complete

--- apriori.py
+++ apriori.py
@@ -16,15 +16,20 @@ def apriori(filename, min_supp):
 
     supports = {}
     count = 0
+    updated_data = []
     data_size = len(data_list)
     for item in unique_data:
         item_count = sum(x.count(item) for x in data_list)
         support = round((item_count / data_size) * 100, 2)
         if support > min_supp:
             supports.update({item: support})
+            updated_data.append(item)
 
-    combs = list(set(it.combinations(unique_data, 2)))
+    combs = list(it.permutations(updated_data, 2))
+    # print(combs)
 
+    updated_combs = []
+    conf = []
     for comb in combs:
         match_count = 0
         for item in data_list:
@@ -34,11 +39,26 @@ def apriori(filename, min_supp):
             support = round((match_count / data_size) * 100, 2)
             if support > min_supp:
                 supports.update({comb: support})
-    print(supports)
+                updated_combs.append(comb)
 
+    sups_and_comps = {}
+    for comb in updated_combs:
+        if set(comb).issubset(supports):
+            conf = round((supports[comb] / supports[comb[0]]) * 100, 2)
+            sups_and_comps.update({comb: conf})
+
+    print(sups_and_comps)
+    print(supports)
+    # data = []
+    # for line in updated_combs:
+    #     for item in line:
+    #         data.append(item)
+    # unique_data = list(dict.fromkeys(data))
+    # combs = list(set(it.combinations(updated_data, 3)))
+    # print(combs)
 
     # filename = input("Enter the name of the transaction file: ")
     # min_supp = int(input("Enter the minimum support value (0 - 100%): "))
     # min_conf = int(input("Enter the minimum confidence value (0 - 100%): "))
     # apriori(filename, min_supp, min_conf)
-apriori('data1', 10)
+apriori('data1', 25)
