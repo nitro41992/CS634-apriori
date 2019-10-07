5f5fd26e45b3cc0f7dfd689857366368b380a9bf
nitro41992 <nick.k@iton.net>
2019-10-07 19:05:14 -0400

    Task: Calculating all combination of associations.

--- apriori.py
+++ apriori.py
@@ -20,11 +20,12 @@ def apriori(filename, min_supp, min_conf):
     i = 1
     prev_count = 0
     confidences = []
-    
+
     while True:
-        print(f'Generating association rules for sets of {i}') if i > 1 else  print(f'Generating association rules for initial set')
+        print(f'Generating association rules for sets of {i}') if i > 1 else print(
+            f'Generating association rules for initial set')
         combs.extend(map(list, (it.permutations(unique_data, i))))
-        
+
         print('Calculating supports...')
         break_counter = prev_count
         for comb in combs:
@@ -44,38 +45,42 @@ def apriori(filename, min_supp, min_conf):
         if prev_count - break_counter == 0:
             break
 
-        
         updated_combs = [item[0] for item in supports]
+        # print(updated_combs)
 
         print('Calculating confidences...')
         confidence = 0
         for comb in updated_combs:
             den = 0
             num = 0
-            if len(comb) > 1:
-                den = [i for i in supports if i[0] == comb[:-1]][0][1]
-                num = [i for i in supports if i[0] == comb][0][1]
-                confidence = round((num / den) * 100, 2)
-                if confidence > min_conf:
-                    confidences.append([comb, confidence])
+            size = len(comb)
+            if size > 1:
+                for pos in range(size - 1, 0, -1):
+                    print(pos)
+                    den = [i for i in supports if i[0] == comb[:-pos]][0][1]
+                    num = [i for i in supports if i[0] == comb][0][1]
+                    confidence = round((num / den) * 100, 2)
+                    if confidence > min_conf:
+                        confidences.append([comb, confidence])
 
         i += 1
 
     print('Generating confidences.csv and supports.csv...')
     with open('supports.csv', 'w', newline='\n', encoding='utf-8') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(['Association','Support(%)'])
+        wr.writerow(['Association', 'Support(%)'])
         wr.writerows(supports)
 
     with open('confidences.csv', 'w', newline='\n', encoding='utf-8') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(['Association','Confidence(%)'])
+        wr.writerow(['Association', 'Confidence(%)'])
         wr.writerows(confidences)
 
 
-filename = input("Enter the name of the transaction file. Include the file extension. (eg. \".txt\") : ")
-min_supp = int(input("Enter the minimum support value (0 - 100%): "))
-min_conf = int(input("Enter the minimum confidence value (0 - 100%): "))
-apriori(filename, min_supp, min_conf)
+# filename = input(
+#     "Enter the name of the transaction file. Include the file extension. (eg. \".txt\") : ")
+# min_supp = int(input("Enter the minimum support value (0 - 100%): "))
+# min_conf = int(input("Enter the minimum confidence value (0 - 100%): "))
+# apriori(filename, min_supp, min_conf)
+apriori('test.txt', 30, 30)
 print('Process completed.')
-
