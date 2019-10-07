6df40e5e91034d0a7ef1b064aa80132ced270399
nitro41992 <nick.k@iton.net>
2019-10-06 20:27:14 -0400

    Task: Calculating supports for combinations complere

--- apriori.py
+++ apriori.py
@@ -20,22 +20,25 @@ def apriori(filename, min_supp):
     for item in unique_data:
         item_count = sum(x.count(item) for x in data_list)
         support = round((item_count / data_size) * 100, 2)
-        supports.update({item: support})
+        if support > min_supp:
+            supports.update({item: support})
 
     combs = list(set(it.combinations(unique_data, 2)))
 
-    item_sets = {}
     for comb in combs:
         match_count = 0
         for item in data_list:
             if set(comb).issubset(item):
                 match_count += 1
         if match_count > 0:
-            item_sets.update({comb: match_count})
+            support = round((match_count / data_size) * 100, 2)
+            if support > min_supp:
+                supports.update({comb: support})
+    print(supports)
 
 
     # filename = input("Enter the name of the transaction file: ")
     # min_supp = int(input("Enter the minimum support value (0 - 100%): "))
     # min_conf = int(input("Enter the minimum confidence value (0 - 100%): "))
     # apriori(filename, min_supp, min_conf)
-apriori('data1', 50)
+apriori('data1', 10)
