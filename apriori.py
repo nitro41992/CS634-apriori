3e8d08cb437ecbe9d7e94d9bd483c040fe97d4bf
nitro41992 <nick.k@iton.net>
2019-10-12 13:31:11 -0400

    Fix: Removed unecessary functions

--- apriori.py
+++ apriori.py
@@ -1,40 +1,16 @@
 import itertools as it
 import csv
-import os.path
 
-# Remove duplicates from supports list
-
-
-# def remove_duplicates(nested_list):
-#     seen_it = set()
-#     upd_list = []
-#     for line in nested_list:
-#         key = frozenset(line[0])
-#         if key not in seen_it:
-#             seen_it.add(key)
-#             upd_list.append(line)
-#     return upd_list
-
-# Pruning and merging based on similarities between same order itemsets
-
-
-def merge(keys, n):
-    resultslist = []
-    lists = list(keys)
-    for x in range(len(lists)):
-        for y in range(x+1, len(lists)):
-            if isinstance(lists[y], str) == False and sorted(lists[x][0:n]) == sorted(lists[y][0:n]) and sorted(lists[x]) != sorted(lists[y]):
-                resultslist.append(lists[x] + lists[y][-1:])
-    return(resultslist)
 
 # create csv from data list
 
 
-def to_csv(filename, output_header, nested_list):
-    with open(filename, 'w', newline='\n', encoding='utf-8') as myfile:
-        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(['Association', output_header])
-        wr.writerows(nested_list)
+def to_csv(filename, dictionary, fieldnames):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        data = [dict(zip(fieldnames, [k, v])) for k, v in dictionary.items()]
+        writer.writerows(data)
 
 # Calculate support for all combinations in datalist anc check minimum support requirements
 
@@ -56,10 +32,6 @@ def check_support(combinations, data_list, min_supp):
             # Support calculation based on matches.
             support = round((match_count / data_size) * 100, 2)
 
-            # Check to append to supports list only if minumum support requirement is met and if row does not already exist
-            # if isinstance(comb, str):
-            #     row = {str(comb): support}
-            # else:
             row = {comb: support}
             if support > min_supp:
                 supports.update(row)
@@ -112,9 +84,7 @@ def apriori(filename, min_supp, min_conf):
         break_count = prev_count
         print(f'Generating association rules for itemsets of {c}...')
 
-        # Runnin merge function as part of the pruning process to create higher order itemsets
-        # print(merge(supports.keys(), c - 2))
-        
+        # Pruning data based on commonalities of attributes
         lists = list(supports.keys())
         n = c-2
         for x in range(len(lists)):
@@ -122,18 +92,15 @@ def apriori(filename, min_supp, min_conf):
                 if isinstance(lists[y], str) == False and sorted(lists[x][0:n]) == sorted(lists[y][0:n]) and sorted(lists[x]) != sorted(lists[y]):
                     row = lists[x] + lists[y][-1:]
                     updated_combs.update({row: None})
-        
-        # updated_combs = merge(supports.keys(), c - 2)
 
         # Re-calculating supports for higher order itemsets
         # Checking to make sure new supports are created for each iteration
         supports.update(check_support(updated_combs, data_list, min_supp)[0])
         prev_count = check_support(updated_combs, data_list, min_supp)[1]
 
-        # upd_supports = remove_duplicates(supports)
-
         # Writing supports to csv
-        # to_csv(f'supports-{filename}.csv', 'Supports(%)', supports)
+        to_csv(f'supports-{filename}.csv', supports,
+               fieldnames=['Itemset', 'Support(%)'])
 
         print(f'Calculating confidences for itemsets of {c}...')
         # Loop through unique itemsets to calculate confidence
@@ -142,7 +109,7 @@ def apriori(filename, min_supp, min_conf):
         for comb in supports.keys():
             den = 0
             num = 0
-            
+
             if isinstance(comb, str):
                 size = 1
             else:
@@ -152,34 +119,27 @@ def apriori(filename, min_supp, min_conf):
                 # Creating permutations based off of itemsets in order to create all association combinations
                 perms = list(it.permutations(comb, len(comb)))
                 # print(perms)
-                
 
                 for i in range(len(perms)):
                     for j in range(1, len(perms[i])):
                         # Isolating the left and right of each association and calculating the confidence
-                       
+
                         # print(keys)
                         for itemset, support in supports.items():
                             if sorted(itemset) == sorted(perms[i][j:]) or itemset == perms[i][j:][0]:
                                 den = support
                             if sorted(itemset) == sorted(perms[i]):
                                 num = support
-                               
+
                         # Making sure confidence meets the minimum requirements
                         confidence = round(((num/den) * 100), 2)
                         if confidence > min_conf:
                             confidences.update(
                                 {f'{perms[i][:j]} -> {perms[i][j:]}': confidence})
 
-        # # Removing redundant confidence permutations
-        # upd_confidences = set(tuple(x) for x in confidences)
-        # b = [list(x) for x in upd_confidences]
-
         # Writing confidences to csv
-        # to_csv(f'confidences-{filename}.csv',
-        #        'Confidence (%)', upd_confidences)
-        print(supports)
-        print(confidences)
+        to_csv(f'confidences-{filename}.csv', confidences,
+               fieldnames=['Association', 'Confidence(%)'])
 
         if prev_count - break_count == 0 or c == max_length:
             break
@@ -209,7 +169,7 @@ def apriori(filename, min_supp, min_conf):
 
 # Running apriori function
 for x in range(1):
-    apriori(f'data{x + 1}.txt', 75, 95)
+    apriori(f'data{x + 1}.txt', 10, 10)
 
 
 print('Process completed.')
